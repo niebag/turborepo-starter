219db3cd9e8d55b3ca3e2497eee1442b2c87b613
Niels Baggerman <niels.baggerman@notive.nl>
2024-01-15 15:18:32 +0100

    build: added eslint repo package

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+    ignorePatterns: ["apps/*", "packages/*"], // This is the base configuration for this monorepo, so the apps and packages are ignored as they should have their own configuration.
+    extends: ["@repo/eslint-config/eslint.base.cjs"],
+}
+
+module.exports = config;
