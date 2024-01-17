c256a49aa679eade4e1e4923603d08a17c0e3e06
Niels Baggerman <niels.baggerman@notive.nl>
2024-01-17 13:45:14 +0100

    style: format

--- apps/next-app-boilerplate/src/app/page.tsx
+++ apps/next-app-boilerplate/src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-import { Button } from "@repo/ui";
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
